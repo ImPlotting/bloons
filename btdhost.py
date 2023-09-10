db795878fa8431b698ee086630afbf8730ccc80d
Ayub Ali <yobo.muhammed@gmail.com>
2023-09-09 22:43:02 -0600

    Add exception to Flask API call

--- btdhost.py
+++ btdhost.py
@@ -7,18 +7,25 @@ app = Flask(__name__)
 @app.route('/')
 
 def get_this_weeks_boss():
-    bosses = requests.get("https://data.ninjakiwi.com/btd6/bosses").json()['body']
-    today = date.today()
-    start_of_week, end_of_week = today - timedelta(today.weekday()), today + timedelta(6 - today.weekday())
-    
-    for boss in bosses:
-        boss_start, boss_end = date.fromtimestamp(boss['start'] / 1000), date.fromtimestamp(boss['end'] / 1000)
+    try:
+        response = requests.get("https://data.ninjakiwi.com/btd6/bosses")
+        response.raise_for_status() #Raise HTTPError if status code was unseccessful
+        bosses = response.json()['body']
         
-        #Check if boss period overlaps
-        if boss_start <= end_of_week and boss_end >= start_of_week:
-            return f"Week's Boss: {boss['name']} ({boss_start} to {boss_end})"
-    
-    return "No boss this week."
+        today = date.today()
+        start_of_week, end_of_week = today - timedelta(today.weekday()), today + timedelta(6 - today.weekday()) #Calculate start of the week and end of the week monday = 0 and sunday = 6
+        
+        for boss in bosses:
+            boss_start, boss_end = date.fromtimestamp(boss['start'] / 1000), date.fromtimestamp(boss['end'] / 1000) #Unix timestamp in miliseconds
+            
+            #Ensure the boss period dosent overlap
+            if boss_start <= end_of_week and boss_end >= start_of_week:
+                return f"Week's Boss: {boss['name']} ({boss_start} to {boss_end})"
+        
+        return "No boss this week."
+
+    except requests.RequestException:
+        return "Error fetching the boss data"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=81)
